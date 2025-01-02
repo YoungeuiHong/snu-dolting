5f7bbd48fba17a18676a2b319e746a63d9288dcb
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-03 00:04:40 +0900

    fix: GNB URL 수정

--- src/components/gnb/index.tsx
+++ src/components/gnb/index.tsx
@@ -6,8 +6,8 @@ export const GnbBar = () => {
     <div className={gnbContainer}>
       <GnbButton src="/icon/home.svg" alt="홈" url="/main" />
       <GnbButton src="/icon/bookmark-gnb.svg" alt="스크랩" url="/scraps" />
-      <GnbButton src="/icon/chat.svg" alt="채팅" url="/chats" />
-      <GnbButton src="/icon/setting.svg" alt="설정" url="/chatting" />
+      <GnbButton src="/icon/chat.svg" alt="채팅" url="/chat" />
+      <GnbButton src="/icon/setting.svg" alt="설정" url="/setting" />
     </div>
   );
 };
