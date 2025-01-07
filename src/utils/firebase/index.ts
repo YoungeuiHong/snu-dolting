4dfb3640e0df13c652fd5c7c6a30cf5f45b60855
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-07 13:57:47 +0900

    fix: 빌드 에러 수정

--- src/utils/firebase/index.ts
+++ src/utils/firebase/index.ts
@@ -1,19 +1,23 @@
 "use client";
 import { initializeApp } from "firebase/app";
-import { getMessaging } from "firebase/messaging";
+import { getMessaging, Messaging } from "firebase/messaging";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAPYz-ViydrRDN2RxUKU7rD1LW-2Wkz25Y",
-  authDomain: "snu-dolting-fd737.firebaseapp.com",
-  projectId: "snu-dolting-fd737",
-  storageBucket: "snu-dolting-fd737.firebasestorage.app",
-  messagingSenderId: "307291527013",
-  appId: "1:307291527013:web:3ef92527361554a9c2338c",
-  measurementId: "G-X5NW20WVG5",
-};
+let app;
+let messaging: Messaging;
 
-const app = initializeApp(firebaseConfig);
+if (typeof window !== "undefined") {
+  const firebaseConfig = {
+    apiKey: "AIzaSyAPYz-ViydrRDN2RxUKU7rD1LW-2Wkz25Y",
+    authDomain: "snu-dolting-fd737.firebaseapp.com",
+    projectId: "snu-dolting-fd737",
+    storageBucket: "snu-dolting-fd737.firebasestorage.app",
+    messagingSenderId: "307291527013",
+    appId: "1:307291527013:web:3ef92527361554a9c2338c",
+    measurementId: "G-X5NW20WVG5",
+  };
 
-const messaging = getMessaging(app);
+  app = initializeApp(firebaseConfig);
+  messaging = getMessaging(app);
+}
 
 export { app, messaging };
