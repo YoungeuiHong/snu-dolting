081a085016628355427b9fba7fe6366d916fb558
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-12 17:28:38 +0900

    fix: notificationclick 이벤트 리스너 수정

--- public/sw.js
+++ public/sw.js
@@ -5,26 +5,3 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", () => {
   console.log("Service Worker activated.");
 });
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  const targetUrl = event.notification.data?.url || "/";
-  event.waitUntil(
-    self.clients
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then((clients) => {
-        for (const client of clients) {
-          if (client.url === new URL(targetUrl, self.location.origin).href) {
-            if ("focus" in client) {
-              return client.focus();
-            }
-          }
-        }
-
-        if (self.clients.openWindow) {
-          return self.clients.openWindow(targetUrl);
-        }
-      }),
-  );
-});
