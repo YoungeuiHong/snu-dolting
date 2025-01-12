02c54fcb8653e0f576e1db09214c79970ddc4d07
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-12 16:27:48 +0900

    chore: notificationclick 이벤트 리스너 위치 이동 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -30,24 +30,3 @@ messaging.onBackgroundMessage((payload) => {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
-
-self.addEventListener("notificationclick", (event) => {
-  event.notification.close();
-
-  const targetUrl = event.notification.data?.url || "/";
-  event.waitUntil(
-    self.clients
-      .matchAll({ type: "window", includeUncontrolled: true })
-      .then((clients) => {
-        for (const client of clients) {
-          if (client.url.includes(targetUrl) && "focus" in client) {
-            return client.focus();
-          }
-        }
-
-        if (self.clients.openWindow) {
-          return self.clients.openWindow(targetUrl);
-        }
-      }),
-  );
-});
--- public/sw.js
+++ public/sw.js
@@ -5,3 +5,24 @@ self.addEventListener("install", () => {
 self.addEventListener("activate", () => {
   console.log("Service Worker activated.");
 });
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  const targetUrl = event.notification.data?.url || "/";
+  event.waitUntil(
+    self.clients
+      .matchAll({ type: "window", includeUncontrolled: true })
+      .then((clients) => {
+        for (const client of clients) {
+          if (client.url.includes(targetUrl) && "focus" in client) {
+            return client.focus();
+          }
+        }
+
+        if (self.clients.openWindow) {
+          return self.clients.openWindow(targetUrl);
+        }
+      }),
+  );
+});
