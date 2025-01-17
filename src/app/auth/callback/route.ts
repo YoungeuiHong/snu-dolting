94d5d6baeb4e31b525bef7735361112237ba0107
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-18 01:50:39 +0900

    fix: 로그인 에러 처리 수정

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
 import { TablesInsert } from "@/types/supabase";
+import { redirect } from "next/navigation";
 
 type UsersInsert = TablesInsert<"users">;
 
@@ -15,8 +16,8 @@ export async function GET(request: Request) {
       await supabase.auth.exchangeCodeForSession(code);
 
     if (sessionError) {
-      // TODO: 로그인 에러 처리
-      console.error("세션 교환 실패:", sessionError);
+      console.error("exchangeCodeForSession 실패: ", sessionError);
+      redirect("/");
     }
 
     // 현재 로그인된 유저 가져오기
@@ -26,8 +27,7 @@ export async function GET(request: Request) {
     } = await supabase.auth.getUser();
 
     if (userError || !user || !user.email) {
-      // TODO: 로그인 에러 처리
-      console.error("유저 정보를 가져오는 데 실패:", userError);
+      redirect("/");
       return;
     }
 
@@ -42,8 +42,8 @@ export async function GET(request: Request) {
       .select();
 
     if (error) {
-      // TODO: 로그인 에러 처리
-      console.error("유저 동기화 실패:", error);
+      console.error("로그인 시 사용자 정보 조회 실패:", error);
+      redirect("/");
     }
 
     if (data?.length && data[0].is_profile_complete) {
@@ -53,6 +53,5 @@ export async function GET(request: Request) {
     }
   }
 
-  // TODO: 로그인 에러 처리
   return NextResponse.redirect(`${origin}`);
 }
