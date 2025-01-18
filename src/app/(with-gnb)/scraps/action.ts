1f78493d9d7cb985d4d6b05c7d3547092ecd051f
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-01-18 22:19:48 +0900

    refactor: 스크랩 목록 조회 로직 개선

--- src/app/(with-gnb)/scraps/action.ts
+++ src/app/(with-gnb)/scraps/action.ts
@@ -3,32 +3,37 @@ import { createClient } from "@/utils/supabase/server";
 import { User } from "@/types/user";
 import { redirect } from "next/navigation";
 
-export async function getScrapList(): Promise<User[]> {
+export async function getScrapList(): Promise<Partial<User>[]> {
   const supabase = await createClient();
 
   const {
     data: { user },
+    error: authError,
   } = await supabase.auth.getUser();
 
-  if (!user) {
+  if (!user || authError) {
     redirect("/login");
   }
 
-  const { data, error } = await supabase
+  const { data: scrapsData, error: scrapsError } = await supabase
     .from("scraps")
     .select(
       `
-      users!scraps_target_user_id_fkey (*)
+      users!scraps_target_user_id_fkey (
+        nickname, profile_picture, birth_year, has_children,
+        height, weight, job, religion, location, introduction
+      )
     `,
     )
     .eq("user_id", user.id);
 
-  if (error) {
-    throw new Error("스크랩 목록 조회에 실패했습니다");
+  if (scrapsError) {
+    console.error("스크랩 목록 조회 실패:", scrapsError.message);
+    throw new Error("스크랩 목록 조회에 실패했습니다.");
   }
 
-  const users: User[] =
-    data?.map((scrap) => scrap.users).filter((user) => user !== null) || [];
+  const users: Partial<User>[] =
+    scrapsData?.map((scrap) => scrap.users).filter((user) => !!user) || [];
 
   return users;
 }
