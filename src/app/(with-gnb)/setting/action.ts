52544c391cd6df646dec7f7d906af50e599a548f
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-02-08 18:16:20 +0900

    fix: 로그아웃 scope local로 수정

--- src/app/(with-gnb)/setting/action.ts
+++ src/app/(with-gnb)/setting/action.ts
@@ -5,7 +5,7 @@ import { redirect } from "next/navigation";
 export async function logout() {
   const supabase = await createClient();
 
-  const { error } = await supabase.auth.signOut();
+  const { error } = await supabase.auth.signOut({ scope: "local" });
 
   if (error) {
     throw Error("로그아웃 실패");
