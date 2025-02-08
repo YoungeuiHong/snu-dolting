c85070436d00c107934f99100adf3f6e9f31ed8a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-02-08 18:10:44 +0900

    fix: 회원가입을 완료한 이후에는 성별 수정 불가능하도록 수정

--- src/app/signup/birthday-gender/form.tsx
+++ src/app/signup/birthday-gender/form.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import {
   container,
   contentContainer,
+  subtitle,
   title,
   titleWrapper,
 } from "@/app/signup/form.css";
@@ -10,6 +11,7 @@ import { updateBirthdayGender } from "@/app/signup/birthday-gender/action";
 import { BinaryChoice, SubmitButton, TextField } from "@/app/signup/components";
 import { useSignupForm } from "@/app/signup/hooks/useSignupForm";
 import { numToString } from "@/utils/type/converts";
+import { useCookie } from "@/hooks/cookie";
 
 interface Props {
   initialGender: string | null;
@@ -22,22 +24,23 @@ export default function Form({ initialGender, initialBirthYear }: Props) {
     birth_year: initialBirthYear,
   });
 
+  const isSignupDone = useCookie("complete");
+
   return (
     <form action={formAction} className={container}>
       <div className={contentContainer}>
         <div className={titleWrapper}>
-          <p className={title}>성별과 출생년도를 입력해주세요</p>
+          <p className={title}>
+            {isSignupDone === "yes" && "출생년도를 입력해주세요"}
+            {isSignupDone === "no" && "출생년도와 성별을 입력해주세요"}
+            &nbsp;
+          </p>
+          {isSignupDone === "no" && (
+            <p className={subtitle}>
+              성별은 이후에 수정할 수 없으니 꼼꼼히 확인해주세요
+            </p>
+          )}
         </div>
-        <BinaryChoice
-          name="gender"
-          label="성별"
-          options={[
-            { value: "male", label: "남성" },
-            { value: "female", label: "여성" },
-          ]}
-          value={state?.user?.gender}
-          error={state?.errors?.gender}
-        />
         <TextField
           name="birth_year"
           label="출생년도"
@@ -45,6 +48,27 @@ export default function Form({ initialGender, initialBirthYear }: Props) {
           value={numToString(state?.user?.birth_year)}
           error={state?.errors?.birth_year}
         />
+        {isSignupDone === "yes" && state?.user?.gender && (
+          <input
+            type="text"
+            name="gender"
+            value={state?.user?.gender}
+            hidden
+            readOnly
+          />
+        )}
+        {isSignupDone === "no" && (
+          <BinaryChoice
+            name="gender"
+            label="성별"
+            options={[
+              { value: "male", label: "남성" },
+              { value: "female", label: "여성" },
+            ]}
+            value={state?.user?.gender}
+            error={state?.errors?.gender}
+          />
+        )}
       </div>
       <SubmitButton pending={pending} />
     </form>
