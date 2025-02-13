9d7d39b85e9de080a8c730ead9b9d2df46e9c89a
Youngeui Hong <snuhongfrcs@snu.ac.kr>
2025-02-13 13:28:14 +0900

    fix: 관리자 페이지 쿼리 조건을 서비스 페이지와 동일하게 수정

--- src/app/(with-gnb)/admin/action.ts
+++ src/app/(with-gnb)/admin/action.ts
@@ -8,7 +8,7 @@ interface GetUsersResponse {
 }
 
 export async function getAllUsers(
-  filters?: UserFilters,
+  filters: UserFilters,
 ): Promise<GetUsersResponse> {
   const supabase = await createClient();
 
@@ -23,12 +23,12 @@ export async function getAllUsers(
   let query = supabase
     .from("users")
     .select(
-      "nickname, profile_picture, introduction, birth_year, has_children, height, weight, job, religion, location, mbti",
-    );
+      "nickname, profile_picture, introduction, birth_year, has_children, height, weight, job, religion, location, is_profile_complete, mbti, remarriage_intent",
+    )
+    .eq("is_profile_complete", true)
+    .neq("email", "snuhongfrcs@snu.ac.kr");
 
-  query = query.neq("id", user.id);
-
-  if (filters?.birthYearRange) {
+  if (filters.birthYearRange) {
     if (filters.birthYearRange.min !== undefined) {
       query = query.gte("birth_year", filters.birthYearRange.min);
     }
@@ -36,16 +36,13 @@ export async function getAllUsers(
       query = query.lte("birth_year", filters.birthYearRange.max);
     }
   }
-  if (
-    filters?.remarriageIntent !== undefined &&
-    filters?.remarriageIntent !== "all"
-  ) {
+  if (filters.remarriageIntent && filters.remarriageIntent !== "all") {
     query = query.eq("remarriage_intent", filters.remarriageIntent);
   }
-  if (filters?.hasChildren !== undefined && filters?.hasChildren !== "all") {
+  if (filters.hasChildren && filters.hasChildren !== "all") {
     query = query.eq("has_children", filters.hasChildren);
   }
-  if (filters?.heightRange) {
+  if (filters.heightRange) {
     if (filters.heightRange.min !== undefined) {
       query = query.gte("height", filters.heightRange.min);
     }
@@ -53,14 +50,15 @@ export async function getAllUsers(
       query = query.lte("height", filters.heightRange.max);
     }
   }
-  if (filters?.religion) {
+  if (filters.religion) {
     query = query.in("religion", filters.religion);
   }
+
+  query.order("created_at", { ascending: false });
 
   const { data: filteredUsers, error } = await query;
 
   if (error) {
-    console.error("사용자 목록 조회 중 에러 발생: ", error);
     throw new Error("사용자 목록 조회에 실패했습니다");
   }
 
